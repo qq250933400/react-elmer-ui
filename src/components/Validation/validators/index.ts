781f19334ee96514e6780e0219566fd0f71315c0
elmer s j mo <250933400@qq.com>
2022-10-22 18:54:03 +0800

    feat: add new validator for validation component

--- src/components/Validation/validators/index.ts
+++ src/components/Validation/validators/index.ts
@@ -1,10 +1,13 @@
 import { isRequired } from "./isRequired";
+import { Callback } from "./Callback";
 
 export type TypeValidators = {
-    isRequired: typeof isRequired
+    isRequired: typeof isRequired;
+    Callback: typeof Callback;
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    isRequired
+    isRequired,
+    Callback
 };
